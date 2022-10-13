def868037836b0e204732d13ca9a2aa3de4c3935
glyaxz <84515834+glyaxz@users.noreply.github.com>
2022-10-13 21:05:37 +0200

    ADD Initial commit

---
+++ app/src/main/java/hl/t1/ejemplo05/MainActivity.java
@@ -0,0 +1,14 @@
+package hl.t1.ejemplo05;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Javier Garcia Ruiz
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
